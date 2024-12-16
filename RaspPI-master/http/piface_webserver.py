eb23901b1cacd134f0a7ace70d0a5773033fadaf
mircebirke1399 <mircolindemann99@gmail.com>
2024-12-16 10:15:46 +0100

    EIN/AUS

--- RaspPI-master/http/piface_webserver.py
+++ RaspPI-master/http/piface_webserver.py
@@ -17,9 +17,27 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     in1=p.digital_read(0)
+    if in1:
+        in1="Ein"
+    else:
+        in1="Aus"
     in2=p.digital_read(1)
+    if in2:
+        in2="Ein"
+    else:
+        in2="Aus"
     in3=p.digital_read(2)
-    in4=p.digital_read(3)   
+    if in3:
+        in3="Ein"
+    else:
+        in3="Aus"
+    in4=p.digital_read(3)
+    if in4:
+        in4="Ein"
+    else:
+        in4="Aus"
+    
+      
     return render_template('index.html',in1=in1,in2=in2,in3=in3,in4=in4)
 
 @app.route('/formular', methods=['POST'])
