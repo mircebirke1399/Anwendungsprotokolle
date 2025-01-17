77d18278d763166dee464fbe7ee09816d4687266
mircebirke1399 <mircolindemann99@gmail.com>
2025-01-17 10:46:12 +0100

    plotly added

--- RaspPI-master/websocket/piface_websocket.py
+++ RaspPI-master/websocket/piface_websocket.py
@@ -1,19 +1,17 @@
 #! /usr/bin/env python3
 
-import time
 import asyncio
-import threading
+import sqlite3
+import json
+import plotly.graph_objs as go
+import plotly.io as pio
 
 from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
 
-import pifacedigitalio
-
-# initialisiere PiFace
-pfd = pifacedigitalio.PiFaceDigital()
-
-# globale Variable zur Speicherung der aktuellen Lauflicht-Geschwindigkeit
-aktuelle_geschwindigkeit = 100
-
+# Connect to SQLite3 database
+DB_FILENAME = 'messungen.db'
+conn = sqlite3.connect(DB_FILENAME, check_same_thread=False, detect_types=sqlite3.PARSE_DECLTYPES)
+cursor = conn.cursor()
 
 class MyServerProtocol(WebSocketServerProtocol):
     verbindungen = [] # weakref.WeakSet() aus import weakref
@@ -27,56 +25,44 @@ class MyServerProtocol(WebSocketServerProtocol):
 
     def onMessage(self, payload, isBinary):
         if isBinary:
-            print("Binary-Nachricht empfangen: {0} Bytes".format(len(payload)))
+            print("Binary message received: {0} bytes".format(len(payload)))
         else:
-            global aktuelle_geschwindigkeit
             message = payload.decode('utf8')
-            print("Text-Nachricht empfangen: {0}".format(message))
-            aktuelle_geschwindigkeit = int(message)
-        # schicke Nachricht als Antwort an Alle zurück
-        for verbindung in self.verbindungen:
-            verbindung.sendMessage(payload, isBinary)
+            print("Text message received: {0}".format(message))
+            if message == "get_plot":
+                self.send_plot()
+
+    def send_plot(self):
+        # Query data from the database
+        cursor.execute("SELECT date, value FROM temp_aussen")
+        rows = cursor.fetchall()
+        
+        # Extract data for plotting
+        dates = [row[0] for row in rows]
+        values = [row[1] for row in rows]
+        
+        # Create a plot using Plotly
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=dates, y=values, mode='lines+markers', name='Temperature'))
+        fig.update_layout(title='Temperature Over Time', xaxis_title='Date', yaxis_title='Temperature (°C)')
+        
+        # Convert plot to HTML
+        plot_html = pio.to_html(fig, full_html=False)
+        
+        # Send plot HTML via WebSocket
+        self.sendMessage(plot_html.encode('utf8'), isBinary=False)
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket-Verbindung geschlossen: {0}".format(reason))
         if self in self.verbindungen:
             self.verbindungen.remove(self)
 
-MAX_VALUE = 128
-INIT_VALUE = 1
-
-def lauflicht_steuerung():
-    global aktuelle_geschwindigkeit
-    aktuelle_Ausgabe = INIT_VALUE
-    aktuelle_Richtung = 0
-    try:
-        while True:
-            # schiebe Ausgabebyte um eine Stelle nach links
-            if aktuelle_Richtung == 0:
-                if aktuelle_Ausgabe >= MAX_VALUE:
-                    aktuelle_Richtung = 1
-                else:
-                    aktuelle_Ausgabe = aktuelle_Ausgabe << 1
-            else:
-                if aktuelle_Ausgabe <= INIT_VALUE:
-                    aktuelle_Richtung = 0
-                else:
-                    aktuelle_Ausgabe = aktuelle_Ausgabe >> 1
-            # schreibe Ausgabebyte auf den Ausgang
-            pfd.output_port.value = aktuelle_Ausgabe
-            print('laufe... {}'.format(aktuelle_Ausgabe))
-            time.sleep(aktuelle_geschwindigkeit / 100.0)
-    except Exception as e:
-        print('Fehler: {}'.format(e))
-        pfd.output_port.value = 0
-
-def start_server(host='127.0.0.1', port=9000):
-    address = 'ws://' + host + ':' + str(port)
-    factory = WebSocketServerFactory(address)
+def start_server():
+    factory = WebSocketServerFactory("ws://127.0.0.1:9000")
     factory.protocol = MyServerProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '0.0.0.0', port)
+    coro = loop.create_server(factory, '127.0.0.1', 9000)
     server = loop.run_until_complete(coro)
 
     try:
@@ -85,13 +71,8 @@ def start_server(host='127.0.0.1', port=9000):
         pass
     finally:
         server.close()
+        loop.run_until_complete(server.wait_closed())
         loop.close()
 
-
-if __name__ == '__main__':
-    # Starte Hintergrund-Thread für das Lauflicht
-    thread = threading.Thread(target=lauflicht_steuerung, args=(), daemon=True)
-    thread.start()
-    # Starte Web-Server zur Steuerung des Lauflichts
-    start_server(host='127.0.0.1', port=9000)
-
+if __name__ == "__main__":
+    start_server()
